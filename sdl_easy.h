f9a189a41e41630c8a6ff6e8a310cb578a860611
a111r <92301153+a111r@users.noreply.github.com>
2022-02-10 22:40:41 +0300

    Create sdl_easy.h

---
+++ sdl_easy.h
@@ -0,0 +1,10 @@
+#ifndef SDL_EASY_H_INCLUDED
+#define SDL_EASY_H_INCLUDED
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+void it_horizontalLine(SDL_Renderer *render, int x, int y, int length);
+void it_verticalLine(SDL_Renderer *render, int x, int y, int length);
+
+#endif // SDL_EASY_H_INCLUDED
