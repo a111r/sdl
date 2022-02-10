f6bbfa40bedb112fb258e77ccbb81694c404ce2e
a111r <92301153+a111r@users.noreply.github.com>
2022-02-10 22:43:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,25 @@
 
 using namespace std;
 
-int main( int argc, char* args[] )
+int main(int argc, char* args[])
 {
     SDL_Renderer *renderer;
     SDL_Window *window;
-    SDL_Init( SDL_INIT_EVERYTHING);
-    SDL_CreateWindowAndRenderer(WINDOW_SIZE, WINDOW_SIZE, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_Init( SDL_INIT_VIDEO);
+
+
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    it_verticalLine(renderer, 0, 1000, 1000);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+    it_horizontalLine(renderer, 500, 500 , 100);
+
     SDL_RenderPresent(renderer);
-    SDL_Delay(1500);
+
+    SDL_RenderClear(renderer);
+    SDL_Delay(3000);
     SDL_Quit();
     return 0;
 }
