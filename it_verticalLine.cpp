a4d9e752f758248beff115376188e93806ae3181
a111r <92301153+a111r@users.noreply.github.com>
2022-02-10 22:41:31 +0300

    Create it_verticalLine.cpp

---
+++ it_verticalLine.cpp
@@ -0,0 +1,15 @@
+#include "sdl_easy.h"
+
+void it_verticalLine(SDL_Renderer *render, int x, int y, int length){
+    int cursorMove;
+    if(length >= 0)
+        cursorMove = -1;
+    else{
+        cursorMove = 1;
+        length *= - 1;
+    }
+    for(int i = 0; i < length; i++){
+        SDL_RenderDrawPoint(render, x, y);
+        y += cursorMove;
+    }
+}
