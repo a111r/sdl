51d64d4de884020a4832b13026f72f7b2f648d5a
a111r <92301153+a111r@users.noreply.github.com>
2022-02-21 10:24:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-#include "sdl_easy.h"
+#include <SDL.h>]
+#include <iostream>
+#include <cmath>
 #define WINDOW_SIZE 1000
+#define WINDOW_WIDTH 1000
 
 using namespace std;
+
+void line_DDA(float x1, float y1, float x2, float y2, SDL_Renderer* renderer)
+{
+    int iX1 = roundf(x1);
+    int iY1 = roundf(y1);
+    int iX2 = roundf(x2);
+    int iY2 = roundf(y2);
+    int deltaX = abs(iX1 - iX2);
+    int deltaY = abs(iY1 - iY2);
+    int lenght = fmax(deltaX, deltaY);
+    if( lenght == 0)
+    {
+        SDL_RenderDrawPoint(renderer, iX1, iY1);
+        return;
+    }
+    double dX = (x2 - x1) / lenght;
+    double dY = (y2 - y1) / lenght;
+    double x = x1;
+    double y = y1;
+    lenght++;
+    while (lenght--)
+    {
+        x += dX;
+        y += dY;
+        SDL_RenderDrawPoint(renderer, roundf(x), roundf(y));
+    }
+}
 
 int main(int argc, char* args[])
 {
@@ -16,8 +46,7 @@ int main(int argc, char* args[])
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-    it_horizontalLine(renderer, 500, 500 , 100);
-
+    line_DDA(250,250, 360,250,renderer);
     SDL_RenderPresent(renderer);
 
     SDL_RenderClear(renderer);
